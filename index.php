7b2b94f907c2bde02fddf429b05ba18b12d87e2f
cuan1210 <quannhph50104@gmail.com>
2025-01-11 22:58:25 +0700

    Layout phần admin

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ require_once './controllers/HomeController.php';
 
 // Require toàn bộ file Models
 require_once './models/Student.php';
+require_once './models/SanPham.php';
 
 // Route
 $act = $_GET['act'] ?? '/';
@@ -16,10 +17,9 @@ $act = $_GET['act'] ?? '/';
 // Để bảo bảo tính chất chỉ gọi 1 hàm Controller để xử lý request thì mình sử dụng match
 
 match ($act) {
-    // Trang chủ
-    // thuan
-    //hallo
     '/' => (new HomeController())->home(),
     
-    'trangchu' => (new HomeController())->trangChu()
+    'trangchu' => (new HomeController())->trangChu(),
+
+    'danh-sach-san-pham' => (new HomeController())->danhSachSanPham(),
 };
